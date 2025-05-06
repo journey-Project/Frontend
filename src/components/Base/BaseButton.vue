f0ccd22a8fac158823a5b001f3c71e7bffb9b6f0
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-05-06 17:36:33 +0900

    feat: BaseButton, BaseInput 수정, BaseSelect 생성, 코드 리펙토링 (#6, #21)
    * feat[#6]: BaseButton BaseInput
    
    * feat[#6]: BaseButton 수정
    
    isActive 부분 수정
    
    * feat[#6]: BaseButton BaseInput 수정
    
    - 버은 글자 색 수정
    - 입력 스타일 수정
    
    * fix[#6]: Base 전역 스타일 변수 사용
    
    * feat[#21]: BaseSelect 추가
    
    * fix: 코드 리펙토링 (#6, #21)

---
+++ src/components/Base/BaseButton.vue
@@ -0,0 +1,94 @@
+<template>
+  <button
+    v-if="isActive"
+    type="button"
+    :class="typeClass + ' ' + _mp"
+    :style="styles"
+    @click="$emit('click')"
+  >
+    {{ _text }}
+  </button>
+  <button v-else disabled type="button" class="btn" :class="_mp" :style="styles">
+    {{ _text }}
+  </button>
+</template>
+
+<script setup>
+import { ref, watchEffect } from 'vue'
+//import { rem } from '@/styles/mixins'
+
+const props = defineProps({
+  _isActive: { type: String, required: true },
+  _text: { type: String, required: true },
+  _textSize: { type: String, required: false, default: '1.8' }, // default 1.8rem
+  _w: { type: String, required: false, default: '10' }, // default 10rem
+  _h: { type: String, required: false, default: '4' }, // default 4rem
+  _style: { type: String, required: true },
+  _mp: { type: String, required: false, default: '' },
+})
+
+const emit = defineEmits(['click'])
+
+const isActive = ref(true)
+const styles = ref('')
+const typeClass = ref('')
+
+watchEffect(() => {
+  isActive.value = props._isActive !== 'false'
+})
+
+watchEffect(() => {
+  const _width = `${parseFloat(props._w)}rem`
+  const _height = `${Math.max(parseFloat(props._h), 2)}rem`
+  const _fontSize = `${parseFloat(props._textSize)}rem`
+
+  styles.value = `width:${_width}; height:${_height}; font-size:${_fontSize}`
+})
+
+watchEffect(() => {
+  if (props._style === 'fill') {
+    typeClass.value = 'btn-fill'
+  } else if (props._style === 'borderline') {
+    typeClass.value = 'btn-borderline'
+  } else {
+    typeClass.value = 'btn-fill'
+  }
+})
+</script>
+
+<style scoped lang="scss">
+@use '@/styles/mixins' as m;
+@use '@/styles/variables' as v;
+
+button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 0;
+  margin: 0 m.rem(10);
+  border: none;
+  border-radius: m.rem(12.5);
+  font-family: inherit;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+button[disabled] {
+  background-color: #f0f0f0;
+  color: gray;
+  border: solid gray m.rem(0.1);
+  cursor: not-allowed;
+}
+
+.btn-fill {
+  color: white;
+  background-color: v.$color-accent;
+}
+
+.btn-borderline {
+  background-color: white;
+  color: v.$color-accent;
+  border: m.rem(1) solid v.$color-accent;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
