8e003876a98547814b2960b5c454e9abcbe86bb6
AshtonSW <sprauncy76@gmail.com>
2025-05-09 00:33:21 +0900

    chore: 전역 설정 및 공통 파일 정리
    - App.vue 및 main.js 기본 구조 정리
    - 전역 변수 (_variables.scss) 추가 및 정리
    - 공통 아이콘 이미지 정리 및 경로 통일
    - router/index.js에 페이지 라우팅 정의

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 import './assets/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'v-calendar/style.css'
 import './styles/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
 
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+setupCalendar(app, {
+  componentPrefix: 'V', // <VDatePicker /> 형식으로 사용 가능
+  locales: { ko: { id: 'ko', firstDayOfWeek: 0 } },
+  theme: false,
+})
+app.component('Calendar', Calendar)
+app.component('VDatePicker', DatePicker)
 
 app.mount('#app')
