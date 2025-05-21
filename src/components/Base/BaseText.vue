73222df39aba22b621e3f11b31bc396d6b200b06
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-05-21 14:26:48 +0900

    feat: BaseText 생성 (#54)

---
+++ src/components/Base/BaseText.vue
@@ -0,0 +1,33 @@
+<template>
+  <component :is="tag" :class="['dynamic-text', { bold }]" :style="textStyle">
+    <slot></slot>
+  </component>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  tag: { type: String, default: 'p' },
+  color: { type: String, default: '--color-primary' },
+  size: { type: String, default: '--fs-body' },
+  bold: { type: Boolean, default: false },
+  lineHeight: { type: String, default: '100%' },
+  letterSpacing: { type: String, default: '0%' },
+})
+
+const textStyle = computed(() => ({
+  color: `var(${props.color})`,
+  fontSize: `var(${props.size})`,
+  fontWeight: props.bold ? 'var(--fw-bold)' : 'var(--fw-regular)',
+  lineHeight: props.lineHeight,
+  letterSpacing: props.letterSpacing,
+  fontFamily: 'var(--font-family)',
+}))
+</script>
+
+<style scoped>
+.dynamic-text {
+  margin: 0;
+}
+</style>
