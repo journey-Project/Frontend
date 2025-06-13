0a1549ce30f21221423f1a1448dbffcf6dc08166
권세림 <117977227+KwonSeRim2@users.noreply.github.com>
2025-06-13 13:04:47 +0900

    [Fix] 회원가입 폼 유효성 검사 개선 및 초기 경고 메시지 수정 (#116)
    * fix: 회원가입 페이지 수정(#115)
    
    - 초기 로딩시 warning메세지 뜨지 않도록 수정(빨간글씨)
    - 아이디 비밀번호 한글 입력 불가능하게
    - 비밀번호 특수문자 표시
    - 이메일 유효성 검사 조건(xxxx@xx.xx)추가

--- src/components/Auth/RegisterForm.vue
+++ src/components/Auth/RegisterForm.vue
@@ -2,7 +2,7 @@
   <div class="formbox">
     <BaseText bold>{{ subtitle }}</BaseText>
     <div class="gap"></div>
-    <LoginForm v-model="modelValue" _type="text" :_ph="_ph" _W="30rem" _H="3rem" />
+    <LoginForm v-model="modelValue" :_type="_type" :_ph="_ph" _W="30rem" _H="3rem" />
     <div class="validation" :class="{ valid: isValid, invalid: !isValid }">
       {{ validationMessage }}
     </div>
@@ -10,11 +10,10 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import BaseText from '../Base/BaseText.vue'
 import LoginForm from './LoginForm.vue'
 
-//v-model을 받을 변수
 const modelValue = defineModel()
 
 const props = defineProps({
@@ -24,20 +23,29 @@ const props = defineProps({
   no: { type: String },
   required: { type: String },
   validator: { type: Function, default: null },
+  _type: { type: String, default: 'text' },
 })
 
-// ✅ 유효성 검사
+// 👉 사용자가 입력을 시작했는지 여부
+const isTouched = ref(false)
+
+watch(modelValue, (newVal) => {
+  if (newVal !== '') {
+    isTouched.value = true
+  }
+})
+
 const isEmpty = computed(() => !modelValue.value)
 const isValid = computed(() => {
   if (isEmpty.value) return false
   if (props.validator) {
     return props.validator(modelValue.value)
   }
-  // 기본 유효성 (예: 영숫자 6~20자)
   return /^[a-zA-Z0-9]{6,20}$/.test(modelValue.value)
 })
 
 const validationMessage = computed(() => {
+  if (!isTouched.value) return '' // 입력 전에는 아무 메시지도 표시하지 않음
   if (isEmpty.value) return props.required
   return isValid.value ? props.ok : props.no
 })
