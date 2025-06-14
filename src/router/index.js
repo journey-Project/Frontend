afaa11845b53d33fa302514992472c5e4e95387b
AshtonSW <sprauncy76@gmail.com>
2025-06-14 22:19:13 +0900

    fix: hotelreservation 경로 수정

--- src/router/index.js
+++ src/router/index.js
@@ -86,7 +86,7 @@ const router = createRouter({
       props: true,
     },
     {
-      path: '/HotelReservation',
+      path: '/hotelreservation',
       name: 'HotelReservation',
       component: HotelReservation,
     },
