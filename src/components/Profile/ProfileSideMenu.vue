78a5221c41e207d5cfcef68f5c3f15ae3f5ec9e4
AshtonSW <sprauncy76@gmail.com>
2025-06-27 11:12:16 +0900

    fix: 프로파일 페이지 왼쪽 사이드 바 메뉴 이름 수정 (#166)

--- src/components/Profile/ProfileSideMenu.vue
+++ src/components/Profile/ProfileSideMenu.vue
@@ -34,9 +34,9 @@ watch(
 
 const menus = [
   { path: '/profile', label: '나의 프로필' },
-  { path: '/profile/follow', label: '팔로우/팔로잉 관리' },
+  { path: '/profile/follow', label: '내 게시글 관리' },
   { path: '/profile/story', label: '스토리 관리' },
-  { path: '/profile/setting', label: '기본 설정' },
+  { path: '/profile/setting', label: '비밀번호 수정' },
 ]
 
 const menuRefs = ref([])
